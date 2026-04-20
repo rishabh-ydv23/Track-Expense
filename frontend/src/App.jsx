82f1aedf1339fe8d12f3309d5a5728aff7c560ec
Rishabh <rishabhyadavunnao@gmail.com>
2026-04-20 22:17:06 +0530

    initialize frontend project with React, Vite, Tailwind CSS, and basic routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Dashboard from "./pages/Dashboard"
+import Layout from "./component/Layout"
+import { useState } from 'react'
 
 export const App = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const navigate = useNavigate();
+  const clearAuth = () => {
+    try {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      sessionStorage.removeItem("token");
+      sessionStorage.removeItem("user");
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  setUser(null);
+  setToken(null);
+
+
+
+  const handleLogout = () => {
+    clearAuth();
+    navigate("/login");
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-      </h1>
-    </div>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
